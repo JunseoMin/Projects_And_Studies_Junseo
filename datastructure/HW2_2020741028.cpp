d18cbc307def86ce8d48017dfb06d0bc1167b4b0
JunseoMin <114414158+JunseoMin@users.noreply.github.com>
2022-12-29 17:32:51 +0900

    Add files via upload

---
+++ datastructure/HW2_2020741028.cpp
@@ -0,0 +1,182 @@
+﻿//visualstudio 2019, windows10
+#include <iostream>
+#include <stack>
+#include <cstring>
+#include <cctype>
+
+using namespace std;
+void clear(stack<double> numbers, stack<char> operations, istream& ins);
+//1. clear istream 
+
+double read_and_evaluate(istream& ins);
+//1. input by cin function
+//2. cheak parenthese
+//3. return evaluated double
+
+void evaluate(stack<double>& numbers,stack<char>& operations);
+// throw error
+// evaluate by stacks
+
+int main()
+{
+	// input onr line- by- oneline
+	// Continue the program until the EOI is entered
+	// Only non-negative double can be entered
+	// 3 types of brackets ({[ can be used
+	// can use 4 binary operators => Priority operation exists
+	// all of real numbers can be output value
+	// Exception output when exception is found (divided by 0, parenthesis count or shape different)
+	// It is assumed that no input is received except for double positive number and operator
+	double res;
+	
+	while (1) {
+		try {
+			res = read_and_evaluate(cin);
+			cout << fixed;
+			cout.precision(3);
+			cout << res << endl;
+		}
+		catch (double except) {
+			cout << "Error!: divided by zero" << endl;
+		}
+		catch (char expt) {
+			cout << "Error!: unbalanced parentheses " << endl;
+		}
+		cin.ignore();
+	}
+}
+
+void clear(stack<double> numbers,stack<char> operations,istream& ins) {
+	while (!numbers.empty())numbers.pop();
+	while (!operations.empty())operations.pop();
+	while (ins.peek() != '\n')ins.ignore();
+}
+
+void evaluate(stack<double>& numbers, stack<char>& operations) {
+	double operand1; double operand2;
+	operand2 = numbers.top();
+	numbers.pop();
+	operand1 = numbers.top();
+	numbers.pop();
+	switch (operations.top()) {
+	case '+':numbers.push(operand1 + operand2); break;
+	case '-':numbers.push(operand1 - operand2); break;
+	case '*':numbers.push(operand1 * operand2); break;
+	case '/':
+		if (operand2 == 0) throw operand2;
+		numbers.push(operand1 / operand2); break;
+	}
+	operations.pop();
+}
+
+double read_and_evaluate(istream& ins) {
+	stack<char> operations;
+	stack<double> numbers;
+
+	double res;
+	double number;
+	char symbol;
+	char parenthes;
+	char err='e';
+
+	while (ins && ins.peek() != '\n')
+	{//push infix to stack
+		if (isdigit(ins.peek()) || ins.peek() == '.') {			//if is number
+			ins >> number;
+			numbers.push(number);
+		}
+		else if (strchr("+-*/", ins.peek()) != NULL) {			//if is operations 
+			ins >> symbol;		
+			if (operations.empty()==true) {						//if operations stack is empty
+				operations.push(symbol);
+			}
+			else {
+				switch (symbol)
+				{												//Consider operators with the same priority
+				case'*':case'/':								
+					operations.push(symbol);
+					break;
+				case'+':case'-':
+					if ((operations.top() == '*') || (operations.top() == '/')) {
+						evaluate(numbers, operations);
+						operations.push(symbol);
+						break;
+					}
+					else operations.push(symbol);
+				}
+			}
+		}
+		else if (ins.peek() == ')') {						//Calculate every time parentheses appear				
+			ins.ignore();									//Ignore right parentheses and calculate until left parentheses appear
+			if (operations.empty())(throw err);
+			while (operations.top() != '(') {
+				if (strchr("{[", operations.top()) != NULL) {// Error output if parentheses are incorrect
+					clear(numbers, operations, ins);		 // clear
+					throw err;								 //err throw -> exit function
+				}
+				evaluate(numbers, operations);
+				if (operations.empty()) {
+					clear(numbers, operations, ins);
+					throw err;
+				}			//If only right parentheses are entered
+			}
+			operations.pop();
+		}
+		else if (ins.peek() == '}') {
+			ins.ignore();
+			if (operations.empty())throw err;
+			while (operations.top() != '{') {
+				if (strchr("([", operations.top()) != NULL) {
+					clear(numbers, operations, ins);
+					throw err;
+				}
+				evaluate(numbers, operations);
+				if (operations.empty()) {
+					clear(numbers, operations, ins);
+					throw err;
+				}
+			}
+			operations.pop();
+		}
+		else if (ins.peek() == ']') {
+			ins.ignore();
+			if (operations.empty())(throw err);
+			while (operations.top() != '[') {
+				if (strchr("({", operations.top()) != NULL) {
+					clear(numbers, operations, ins);
+					throw err;
+				}
+				evaluate(numbers, operations);
+				if (operations.empty()) {
+					clear(numbers, operations, ins);
+					throw err;
+				}
+			}
+			operations.pop();
+		}
+		else if (strchr("({[", ins.peek()) != NULL) {		// If left parentheses
+			ins >> parenthes;
+			operations.push(parenthes);
+		}
+		else if (ins.peek() == 'E') {
+			ins.ignore();
+			if (ins.peek() == 'O') {
+				ins.ignore();
+				if (ins.peek() == 'I') {	//Exit the program when the EOI is entered
+					exit(true);
+				}
+			}
+		}
+		else ins.ignore();				
+	}
+	while (!operations.empty()) {			//Processing the remaining calculations
+		switch (operations.top()) {
+		case '(':case '{':case '[':
+			clear(numbers, operations, ins);
+			throw err;
+		default:
+			evaluate(numbers, operations); break;
+		}
+	}
+	return numbers.top();
+}
---
+++ datastructure/HW____3.cpp
@@ -0,0 +1,221 @@
+﻿//2020741028 민준서
+//visualstudio 2019
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <format>
+#include <cmath>
+
+#define U 0
+#define D 1
+#define R 2
+#define L 3
+
+using namespace std;
+
+int center(int n) { return n <= 1 ? 0 : 2 * center(n / 4) + 1; }
+int depth(int n) { return n <= 7 ? 1 : 2 * depth(n / 4); }
+
+void make_vec(istream& ins, vector<char>& invec);
+void rotated(const vector<char>& invec,int i,int depth);
+void not_rotated(const vector<char>& invec, int depth);
+void Del_Heap(vector<char>& invec,const char target);
+char** H(char** H_tree, const vector<char>& invec, int node_index, int i, int j, int d, int up, int down, int right, int left);
+void h_tree(char**& H_tree, const vector<char>& invec, int size);
+char** H_size(int size);
+
+int main()
+{
+	vector<char> invec;
+
+	while (1)
+	{
+		try
+		{
+			make_vec(cin,invec);											//get vector and make B-tree
+		}
+		catch (bool)
+		{
+			cout << "!!ERROR:: USE INS # OR EOI";
+		}
+		catch (int) {
+			cout << "!!ERROR:: NONE DEL";
+		}
+		cin.ignore();
+	}
+}
+
+void make_vec(istream& ins, vector<char>& invec) {
+	char inchar;
+	int full_depth=1;
+	int tree_size=0;
+	int h_3_size=1;
+	while (ins && ins.peek() != '\n') {
+		if (ins.peek() == 'E') {
+			ins.ignore();
+			if (ins.peek() == 'O') {
+				ins.ignore();
+				if (ins.peek() == 'I') {	
+					make_heap(invec.begin(), invec.end());
+					for (int i = 1; i < invec.size(); i++)			//full depth of tree
+						if (i == pow(2, full_depth) - 1) {
+							++full_depth;
+						}
+					--full_depth;
+					cout << "1. Rotated form  "<<endl;
+					rotated(invec,0,0);
+					cout << "2. Not-Rotated form "<<endl;
+					not_rotated(invec,full_depth);
+					cout << "3. H-tree form "<<endl;
+					for (int k = 1; k < depth(invec.size()); k++)
+						if (k == pow(2, h_3_size) - 1) {
+							++h_3_size;
+						}
+					tree_size = pow(2, h_3_size + 1) - 1;
+					char** H_tree = H_size(tree_size);
+					h_tree(H_tree, invec, tree_size);
+					break;
+				}
+			}
+		}
+		else if (ins.peek() == 'I') {
+			ins.ignore();
+			if (ins.peek() == 'N') {
+				ins.ignore();
+				if (ins.peek() == 'S') {
+					{
+						ins.ignore();
+						ins >> inchar;
+						invec.push_back(inchar);
+						if (invec.size() == 200) {
+							exit(true);
+						}
+					}
+				}
+			}
+		}
+		else if (ins.peek() == 'E') {	// 실수로 선택 노드 지우는 함수 만들었는데 아쉬워서 납뒀습니다...
+			ins.ignore();
+			if (ins.peek() == 'R') {
+				ins.ignore();
+				if (ins.peek() == 'S') {
+					ins.ignore();
+					ins >> inchar;
+					Del_Heap(invec, inchar);
+				}
+			}
+		}
+		else if (ins.peek() == 'D') {	
+			ins.ignore();
+			if (ins.peek() == 'E') {
+				ins.ignore();
+				if (ins.peek() == 'L') {
+					ins.ignore();
+					make_heap(invec.begin(), invec.end());
+					pop_heap(invec.begin(), invec.end());
+					invec.pop_back();
+				}
+			}
+		}
+		else if(ins.peek()==' ')
+		{
+			ins.ignore();
+		}
+		else {
+					throw false;
+		}
+	}
+}
+void rotated(const vector<char>& invec,const int i ,const int depth) {
+	//Reculsive Function
+	//Right child=index*2+2
+	//left child = index*2+1
+	if (i<invec.size()) {
+		rotated(invec, 2 * i + 1, depth + 1);
+		cout.width(depth*2);
+		cout << invec[i] << endl;
+		rotated(invec, 2 * i + 2, depth + 1);
+	}
+}
+void not_rotated(const vector<char>& invec, int full_depth) {
+	int i=0;
+	int k = 0;		//k=depth
+	int fd = full_depth;
+	while (true)
+	{
+		i = k;
+		for (int l = 0; l < i + 1; l++) {
+			if (k == invec.size()) {
+				cout << endl;
+				return;
+			}
+			if ((i + l) % 2 != 0)cout.width(pow(2, full_depth));
+			else if ((i + l) == 0)cout.width(pow(2,full_depth)-1);
+			else cout.width(pow(2, full_depth) + 1);
+			cout  << invec[i + l];
+			k++;
+		}
+		cout << endl;
+		full_depth--;
+	}
+}
+
+char** H_size(int size) {
+	char** arr = new char* [size];
+	for (int i = 0; i < size; ++i) {
+		arr[i] = new char[size];
+		memset(arr[i], 0, sizeof(char) * size);
+	}
+	return arr;
+}
+
+char** H(char** H_tree,const vector<char>& invec, int node_index, int i, int j, int d, int up, int down, int ight, int left) {
+	int V[4][2] = { { -1, 0 },{ 1, 0 },{ 0, 1 },{ 0, -1 } };
+	if (node_index > invec.size()) return NULL;
+	H_tree[i][j] = invec[node_index - 1];
+	if (2 * node_index <= invec.size()) {
+		H_tree[i + d * V[L][0]][j + d * V[L][1]] = invec[2 * node_index - 1];
+		H(H_tree, invec, 4 * node_index, i + d * (V[L][0] + V[U][0]),
+			j + d * (V[L][1] + V[U][1]), d / 2, D, U, L, R);
+		H(H_tree, invec, 4 * node_index + 1, i + d * (V[L][0] + V[D][0]),
+			j + d * (V[L][1] + V[D][1]), d / 2, U, D, R, L);
+	}
+	if (2 * node_index + 1 <= invec.size()) {
+		H_tree[i + d * V[R][0]][j + d * V[R][1]] = invec[2 * node_index];
+		H(H_tree, invec, 4 * node_index + 2, i + d * (V[R][0] + V[D][0]),
+			j + d * (V[R][1] + V[D][1]), d / 2, U, D, R, L);
+		H(H_tree, invec, 4 * node_index + 3, i + d * (V[R][0] + V[U][0]),
+			j + d * (V[R][1] + V[U][1]), d / 2, D, U, L, R);
+	}
+	return H_tree;
+}
+
+void h_tree(char**& H_tree,const vector<char>& invec, int size) {
+	char** htree = H(H_tree, invec, 1, center(invec.size()), center(invec.size()), depth(invec.size()), 0, 1, 2, 3);
+	for (int i = 0; i < size; ++i) {
+		for (int j = 0; j < size; ++j) {
+			if (j == 0)
+				cout.width(1);
+			else
+				cout.width(2);
+			cout << htree[i][j];
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < size; ++i) { delete[] htree[i]; } delete[] htree;
+}
+
+void Del_Heap(vector<char>& invec, const char target) {
+	int i = 0;
+	while (i<=invec.size()) {
+		if (invec[i] == target) {
+			invec.erase(invec.begin()+i);
+			make_heap(invec.begin(), invec.end());
+			return;
+		}
+		i++;
+	}
+	if (i == invec.size()) {
+		throw i;
+	}
+}
