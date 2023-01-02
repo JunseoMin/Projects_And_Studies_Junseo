31af9e349b86fc4709ee2b0eeafd5f6a73ce9df7
JunseoMin <minjs4562@gmail.com>
2023-01-02 19:43:43 +0900

    Update Dynamic_Array.cpp

--- DataStructure&Algorithms/Contiguious_DataStructure/Dynamic_Array.cpp
+++ DataStructure&Algorithms/Contiguious_DataStructure/Dynamic_Array.cpp
@@ -4,20 +4,23 @@
 #include "Dynamic_Array.h"
 
 using namespace std;
-// »ç¿ëÀÚ·ÎºÎÅÍ sizeÀÔ·Â¹ÞÀ½(sizeÇÒ´ç)
+// ï¿½ï¿½ï¿½ï¿½Ú·Îºï¿½ï¿½ï¿½ sizeï¿½Ô·Â¹ï¿½ï¿½ï¿½(sizeï¿½Ò´ï¿½)
 template<typename T>
 Dynamic_Array<T>::Dynamic_Array(int data_size) {
 	this->data_size = data_size;
-	data_ptr=new data_ptr[data_size];	//ÁÖ¼Ò ÇÒ´ç
+	data_ptr=new T[data_size];	//ï¿½Ö¼ï¿½ ï¿½Ò´ï¿½
 }
 
 template<typename T>
-Dynamic_Array<T>::Dynamic_Array(const Dynamic_Array<T>& array) {
-	// data_ptr = array.data_ptr; !!»õ·Î ÇÒ´çÇØ¾ß º¹»ç ´çÇÏ´Â arrayÀÇ °ª¿¡ ¿µÇâx
+Dynamic_Array<T>::Dynamic_Array( Dynamic_Array<T>& array) {
+	// data_ptr = array.data_ptr; !!ï¿½ï¿½ï¿½ï¿½ ï¿½Ò´ï¿½ï¿½Ø¾ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ï´ï¿½ arrayï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½x
 	data_size = array.data_size;
-	for (auto ele : array.data_ptr)
-		data_ptr[ele] = array.data_ptr[ele];
-// ¹üÀ§ÁöÁ¤ ¿¬»ê µÇ´ÂÁö È®ÀÎÇÏ±â!! - 22.12.29
+	data_ptr = new T[data_size];
+	
+	for (int i = 0; i < data_size; i++) {
+		data_ptr[i] = array[i];
+	}
+// ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½Ç´ï¿½ï¿½ï¿½ È®ï¿½ï¿½ï¿½Ï±ï¿½!! - 22.12.29
 }
 
 template<typename T>
@@ -36,11 +39,54 @@ const T& Dynamic_Array<T>::operator[](const int& index) const {
 }
 
 template<typename T>
-T& Dynamic_Array <T>::at(const int index) {
+T& Dynamic_Array <T>::at(const int index) {	// stdï¿½Ô¼ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ at()ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ëµµ => ï¿½ï¿½ï¿½ï¿½Ã³ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
 	if (index < data_size) return data_ptr[index];
-	throw "Index out of range";	//class ¼±¾ðºÎ¿¡¼­ ¹ß»ýÇÒ ¼ö ÀÖ´Â ¿¹¿ÜÃ³¸®¹æ¹ý
+	throw "Index out of range";							//main ï¿½ï¿½ï¿½ï¿½ ï¿½ß»ï¿½ï¿½ï¿½ ï¿½ï¿½ ï¿½Ö´ï¿½ ï¿½ï¿½ï¿½ï¿½Ã³ï¿½ï¿½ï¿½ï¿½ï¿½
 }
+
 template<typename T>
 size_t Dynamic_Array <T>::size() {
 	return data_size;
+}
+
+template<typename T>
+T& Dynamic_Array<T>::begin() {
+	return data_ptr;
+}
+
+template<typename T>
+const T& Dynamic_Array <T>::begin() const {
+	return data_ptr;
+}
+
+template<typename T>
+T& Dynamic_Array <T>::end() {
+	return data_ptr + data_size;
+}
+
+template<typename T>
+const T& Dynamic_Array <T>::end() const {
+	return data_ptr + data_size;
+}
+
+template<typename T>
+Dynamic_Array<T> operator+(const Dynamic_Array<T>& arr1,const Dynamic_Array<T>& arr2) {
+	Dynamic_Array<T> Ret_Arr (arr1.size()+arr2.size());	//ï¿½ï¿½ï¿½ï¿½ï¿½Çµï¿½ operator +ï¿½ï¿½ ï¿½Æ´ï¿½ sizetype ï¿½ï¿½ sumï¿½ï¿½
+	copy(arr1.begin(), arr1.end(), Ret_Arr.begin());
+	copy(arr2.begin(), arr2.end(), Ret_Arr.begin()+arr1.size());
+
+	return Ret_Arr;
+}
+
+template<typename T>
+string Dynamic_Array<T>::to_string(const string& sep)
+{
+	if (data_size == 0) return "";
+
+	ostringstream os;
+	for (int i = 0; i < data_size; i++)
+	{
+		os << data_ptr[i] << sep;
+	}
+	return os.str();
 }
