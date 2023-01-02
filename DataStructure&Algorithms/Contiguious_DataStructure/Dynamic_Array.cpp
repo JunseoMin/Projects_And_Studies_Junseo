6872127559586b46e5f7e461679016e3f1cb6834
JunseoMin <minjs4562@gmail.com>
2023-01-02 19:45:20 +0900

    Update Dynamic_Array.cpp

--- DataStructure&Algorithms/Contiguious_DataStructure/Dynamic_Array.cpp
+++ DataStructure&Algorithms/Contiguious_DataStructure/Dynamic_Array.cpp
@@ -4,23 +4,23 @@
 #include "Dynamic_Array.h"
 
 using namespace std;
-// ����ڷκ��� size�Է¹���(size�Ҵ�)
+// 사용자로부터 size입력받음(size할당)
 template<typename T>
 Dynamic_Array<T>::Dynamic_Array(int data_size) {
 	this->data_size = data_size;
-	data_ptr=new T[data_size];	//�ּ� �Ҵ�
+	data_ptr=new T[data_size];	//주소 할당
 }
 
 template<typename T>
 Dynamic_Array<T>::Dynamic_Array( Dynamic_Array<T>& array) {
-	// data_ptr = array.data_ptr; !!���� �Ҵ��ؾ� ���� ���ϴ� array�� ���� ����x
+	// data_ptr = array.data_ptr; !!새로 할당해야 복사 당하는 array의 값에 영향x
 	data_size = array.data_size;
 	data_ptr = new T[data_size];
 	
 	for (int i = 0; i < data_size; i++) {
 		data_ptr[i] = array[i];
 	}
-// �������� ���� �Ǵ��� Ȯ���ϱ�!! - 22.12.29
+// 범위지정 연산 되는지 확인하기!! - 22.12.29
 }
 
 template<typename T>
@@ -39,9 +39,9 @@ const T& Dynamic_Array<T>::operator[](const int& index) const {
 }
 
 template<typename T>
-T& Dynamic_Array <T>::at(const int index) {	// std�Լ������� at()�� ������ �뵵 => ����ó���� ����
+T& Dynamic_Array <T>::at(const int index) {	// std함수에서의 at()과 동일한 용도 => 예외처리에 유용
 	if (index < data_size) return data_ptr[index];
-	throw "Index out of range";							//main ���� �߻��� �� �ִ� ����ó�����
+	throw "Index out of range";							//main 에서 발생할 수 있는 예외처리방법
 }
 
 template<typename T>
@@ -71,7 +71,7 @@ const T& Dynamic_Array <T>::end() const {
 
 template<typename T>
 Dynamic_Array<T> operator+(const Dynamic_Array<T>& arr1,const Dynamic_Array<T>& arr2) {
-	Dynamic_Array<T> Ret_Arr (arr1.size()+arr2.size());	//�����ǵ� operator +�� �ƴ� sizetype �� sum��
+	Dynamic_Array<T> Ret_Arr (arr1.size()+arr2.size());	//재정의된 operator +가 아닌 sizetype 의 sum값
 	copy(arr1.begin(), arr1.end(), Ret_Arr.begin());
 	copy(arr2.begin(), arr2.end(), Ret_Arr.begin()+arr1.size());
 
